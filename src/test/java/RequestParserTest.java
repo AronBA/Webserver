f7be0f3e063e9cb2d152bd01b9ca2bc47bfb9272
Aron Baur <aron.baur@bs.ch>
2024-04-02 13:37:47 +0200

    adds test dependencies

---
+++ src/test/java/RequestParserTest.java
@@ -0,0 +1,14 @@
+import org.junit.Test;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+public class RequestParserTest {
+
+
+    @Test
+    public void thisIsATest(){
+        int result = 10;
+        int expected = 10;
+        assertThat(result).isEqualTo(expected);
+    }
+}
