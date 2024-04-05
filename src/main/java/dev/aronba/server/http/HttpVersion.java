eaec7bf06dcf9e6d293ab90a0e044bd2cf16195a
Aron Baur <aron.baur@bs.ch>
2024-04-05 15:28:43 +0200

    delombok

--- src/main/java/dev/aronba/server/http/HttpVersion.java
+++ src/main/java/dev/aronba/server/http/HttpVersion.java
@@ -1,11 +1,5 @@
 package dev.aronba.server.http;
 
-
-import lombok.Getter;
-import lombok.RequiredArgsConstructor;
-import lombok.ToString;
-
-
 public enum HttpVersion {
     HTTP_1_0("HTTP/1.0"),
     HTTP_1_1("HTTP/1.1"),
